0e39c8d81b128d734b1548f19f5d7a245e884da6
ababanin <aleksey.babanin@gmail.com>
2022-04-08 12:44:03 +0300

    test coverage

---
+++ src/test/java/icu/cyclone/bancleaner/service/BanServiceTest.java
@@ -0,0 +1,68 @@
+package icu.cyclone.bancleaner.service;
+
+import icu.cyclone.bancleaner.configuration.properties.StorageProperties;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static icu.cyclone.bancleaner.TestObjects.TEMPLATE_HOST_INFO;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(classes = {BanService.class})
+@ExtendWith(SpringExtension.class)
+class BanServiceTest {
+
+    @Autowired
+    public BanService banService;
+
+    @MockBean
+    public HostInfoService hostInfoService;
+
+    @MockBean
+    public Fail2BanService fail2BanService;
+
+    @MockBean
+    public UnbanDecisionService decisionService;
+
+    @Test
+    public void autoCleanWithoutUnbanTest() {
+        when(fail2BanService.fetchBannedIpSet()).thenReturn(Set.of("8.8.8.8"));
+        when(hostInfoService.getHostInfo(anyString())).thenReturn(TEMPLATE_HOST_INFO);
+        when(decisionService.isUnban(any())).thenReturn(false);
+
+        banService.autoClean();
+
+        verify(fail2BanService, times(1)).fetchBannedIpSet();
+        verify(hostInfoService, times(1)).getHostInfo(anyString());
+
+        verify(fail2BanService, times(0)).unban(anyString());
+        verify(hostInfoService, times(0)).unbanCountIncrement(any());
+        verify(hostInfoService,times(1)).storeDatabase();
+    }
+
+    @Test
+    public void autoCleanWithUnbanTest() {
+        when(fail2BanService.fetchBannedIpSet()).thenReturn(Set.of("8.8.8.8"));
+        when(hostInfoService.getHostInfo(anyString())).thenReturn(TEMPLATE_HOST_INFO);
+        when(decisionService.isUnban(any())).thenReturn(true);
+
+        banService.autoClean();
+
+        verify(fail2BanService, times(1)).fetchBannedIpSet();
+        verify(hostInfoService, times(1)).getHostInfo(anyString());
+
+        verify(fail2BanService, times(1)).unban(anyString());
+        verify(hostInfoService, times(1)).unbanCountIncrement(any());
+        verify(hostInfoService,times(1)).storeDatabase();
+    }
+}
--- src/test/java/icu/cyclone/bancleaner/service/HostInfoServiceTest.java
+++ src/test/java/icu/cyclone/bancleaner/service/HostInfoServiceTest.java
@@ -3,6 +3,9 @@ package icu.cyclone.bancleaner.service;
 import icu.cyclone.bancleaner.configuration.properties.StorageProperties;
 import icu.cyclone.bancleaner.dataservice.HostInfoDataService;
 import icu.cyclone.bancleaner.domain.HostInfo;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -14,8 +17,10 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static icu.cyclone.bancleaner.TestObjects.TEMPLATE_HOST_INFO;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(classes = {HostInfoService.class})
 @EnableConfigurationProperties(StorageProperties.class)
@@ -51,5 +56,47 @@ class HostInfoServiceTest {
 
         verify(dataService).save(argument.capture());
         assertEquals(TEMPLATE_HOST_INFO.getUnbannedCount() + 1, argument.getValue().getUnbannedCount());
+    }
+
+    @Test
+    public void getHostInfoTest() {
+        when(dataService.find(anyString())).thenReturn(Optional.of(TEMPLATE_HOST_INFO));
+        var calendar = new GregorianCalendar(2022, 0 , 1);
+        calendar.add(Calendar.DAY_OF_MONTH, -1);
+        when(calendarService.getCalendar()).thenReturn(calendar);
+        var result = hostInfoService.getHostInfo("8.8.8.8");
+        assertEquals(TEMPLATE_HOST_INFO, result);
+        assertEquals(TEMPLATE_HOST_INFO.getUnbannedCount(), result.getUnbannedCount());
+        assertEquals(TEMPLATE_HOST_INFO.getFirstBannedDate(), result.getFirstBannedDate());
+        assertEquals(TEMPLATE_HOST_INFO.getLastUpdateDate(), result.getLastUpdateDate());
+    }
+
+    @Test
+    public void getHostInfoFirstTest() {
+        when(dataService.find(anyString())).thenReturn(Optional.empty());
+        var calendar = new GregorianCalendar(2022, 1 , 1);
+        when(calendarService.getCalendar()).thenReturn(calendar);
+        when(whoIsService.getHostInfo(anyString())).thenReturn(TEMPLATE_HOST_INFO);
+        var result = hostInfoService.getHostInfo("8.8.8.8");
+        assertEquals(TEMPLATE_HOST_INFO.getIp(), result.getIp());
+        assertEquals(TEMPLATE_HOST_INFO.getUnbannedCount(), result.getUnbannedCount());
+        assertEquals(calendar, result.getFirstBannedDate());
+        assertEquals(TEMPLATE_HOST_INFO.getLastUpdateDate(), result.getLastUpdateDate());
+    }
+
+    @Test
+    public void getHostInfoExpiredTest() {
+        when(dataService.find(anyString())).thenReturn(Optional.of(TEMPLATE_HOST_INFO));
+        var calendar = new GregorianCalendar(2022, 0 , 1);
+        calendar.add(Calendar.DAY_OF_MONTH, 32);
+        when(calendarService.getCalendar()).thenReturn(calendar);
+        when(whoIsService.getHostInfo(anyString()))
+            .thenReturn(TEMPLATE_HOST_INFO.toBuilder().lastUpdateDate(calendar).build());
+
+        var result = hostInfoService.getHostInfo("8.8.8.8");
+        assertEquals(TEMPLATE_HOST_INFO.getIp(), result.getIp());
+        assertEquals(TEMPLATE_HOST_INFO.getUnbannedCount(), result.getUnbannedCount());
+        assertEquals(TEMPLATE_HOST_INFO.getFirstBannedDate(), result.getFirstBannedDate());
+        assertEquals(calendar, result.getLastUpdateDate());
     }
 }
